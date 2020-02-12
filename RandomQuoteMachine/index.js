aec46aeb51d7e0be350c5d5cd3ba7a47431794ff
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-12 12:20:45 -0500

    apply getQuotes after document is ready

--- RandomQuoteMachine/index.js
+++ RandomQuoteMachine/index.js
@@ -58,3 +58,15 @@ function getQuote() {
         }
     );
 }
+
+$(document).ready(function() {
+    getQuotes().then(() => {
+        getQuote();
+    });
+
+    $('#new-quote').on('click', getQuote);
+
+    $('#tweet-quote').on('click', function() {
+        openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    });
+});
