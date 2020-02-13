44d9daf10b9de202904cfff9e3612a0069b4f274
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-13 12:56:18 -0500

    add

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
 
+const Toolbar = (props) => {
+  return (
+    <div className="toolbar">
+      <i title="no-stack-dub-sack" className="fa fa-free-code-camp"/>      
+      {props.children}
+      <i onClick={props.onClick} className={props.icon}></i>
+    </div>
+ )
+};
+
 function App() {
   return (
-    <div className="App">
-      <div>
+    <div>
+      <div className="editorWrap maximized">
         <p>Toolbar + Editor</p>
+        <Toolbar icon="fa fa-arrows-alt">
+                Editor
+        </Toolbar>
       </div>
-      <div>
-      <p>Toolbar + Previewer</p>
+      <div className="previewWrap">
+        <p>Toolbar + Previewer</p>
+        <Toolbar icon="editorWrap maximized">
+              Previewer
+        </Toolbar>
       </div>
     </div>
   )
