06dae510d5917988dd1ddb38461d94164e0c5cb3
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-13 13:07:46 -0500

    update preview styles

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -15,13 +15,11 @@ function App() {
   return (
     <div>
       <div className="editorWrap maximized">
-        <Toolbar icon="fa fa-arrows-alt">
-                Editor
+        <Toolbar icon="fa fa-arrows-alt" text="Editor">
         </Toolbar>
       </div>
-      <div className="editorWrap maximized">
-        <Toolbar icon="'fa fa-arrows-alt">
-              Previewer
+      <div className="previewWrap maximized">
+        <Toolbar icon="'fa fa-arrows-alt" text="Previewer">
         </Toolbar>
       </div>
     </div>
