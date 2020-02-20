1326792150a0a0eb7df5dbdb2d4b442fbeafc9fe
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-20 10:02:40 -0500

    add basic timer

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -4,11 +4,32 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      time: 60
+    };
+    this.startTimer = this.startTimer.bind(this)
+    this.stopTimer = this.stopTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => this.setState({
+      time: this.state.time - 1
+    }), 1000)
+  }
+  stopTimer() {
+    clearInterval(this.timer)
+  }
+  resetTimer() {
+    this.setState({time: 60})
   }
   render() {
     return (
       <div>
-        <p>Hello World!</p>
+        <h3>timer: {this.state.time}</h3>
+        <button onClick={this.startTimer}>start</button>
+        <button onClick={this.stopTimer}>stop</button>
+        <button onClick={this.resetTimer}>reset</button>
       </div>
     )
   }
